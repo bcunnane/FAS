04b44bf10892173855db3cb694d8b27e9554d2f7
bcunnane <89225986+bcunnane@users.noreply.github.com>
2021-09-23 12:02:59 -0700

    updated to colormap strain, not diffusion
    Updated ev_colormap fn to plot general colormaps of eignevector data. Updated main script to process and plot colormaps of strain, not diffusion data.

--- fas_analysis_main.m
+++ fas_analysis_main.m
@@ -1,46 +1,65 @@
 %% FAS analysis main
 %   main script for fiber aligned strain & strain rate analysis
-
 %load('Results.mat')
 
-for n = 1:length(Data)
-    ev_colormaps(Data(n).dti_vec, char(Data(n).MVC))
-end
+%% Strain (St) & Strain Rate (SR) colormaps
 
-%% functions
-function ev_colormaps(ev_data, pct_MVC)
-% creates & saves DTI eigen vector (ev) colormaps in current directory
-% expects ev_data matrix as [pix, pix, slices, 3 vectors, 3 directions]
-
-% re-arrange eigen vector data
-all_evs=[];
-for k = 1:3
-    ev = squeeze(ev_data(:,:,:,:,k)); % select only 1 ev
-    ev = permute(ev,[1,2,4,3]); % swap slice & ev locations in matrix
-    ev = ev(:,:,[1,3,2],:); % reorder ev directions from xyz to xzy
-    ev = abs(ev);
+% generate colormaps
+source = 'SR'; %strain data source
+ev_labels = ["x1","x2","x3";"y1","y2","y3";"z1","z2","z3"];
+for n = 1%:length(Data)
+    % trim ev matrix
+    data = squeeze(Data(n).strain.SR_vector); % change strain data source
     
-    all_evs = cat(4,all_evs,ev); % compile all slices into 1 matrix
-end
-
-% display colormaps with thumbnails as original image size
-montage(all_evs, 'ThumbnailSize', [])
-
-% add text labels
-dirs = 'XZY';
-npix = size(all_evs, 1);
-for i = 1:3
-    for j = 1:3
-        x = npix .* [0:2] + 3;
-        y = npix .* [1:3] - 9;
-        ev_name = ['ev',num2str(j),'  ',dirs(i)];
-        text(x(i), y(j), ev_name, 'Color', 'w')
+    % create colormaps
+    num_frs = size(data,3);
+    pct_mvc = char(Data(n).MVC);
+    for fr = 1:num_frs
+        name = sprintf('%s %s%% MVC Frame %d of %d', source, pct_mvc, fr, num_frs);
+        ev_colormap(squeeze(data(:,:,fr,:,:)), ev_labels, name)
     end
 end
-text(3,12,[pct_MVC,'% MVC'], 'Color', 'w')
+
+% organize images
+mkdir(source)
+movefile('*.png',source)
+
+%% functions
+function ev_colormap(evs, labels, name)
+% expects eigenvectors (evs) matrix as [pix, pix, 3 vectors, 3 directions]
+% assumes eigen vector matrix is:
+%   x1   x2   x3
+%   y1   y2   y3
+%   z1   z2   z3
+% but montage function plots as:
+%   1    2    3
+%   4    5    6
+%   7    8    9
+% ev order is therefore altered so that the montage image matches the ev 
+% matrix convention
+
+% plot colormap montage
+evs = abs(evs(:,:,:));
+evs = evs(:,:,[1 4 7 2 5 8 3 6 9]); % change ev order so montage matches matrix
+montage(evs, 'ThumbnailSize', [])
+colormap('jet')
+colorbar
+
+% add text labels
+npix = size(evs, 1);
+x = npix .* [0:2] + 3;
+y = npix .* [1:3] - 9;
+x = x([1 1 1 2 2 2 3 3 3]);
+y = y([1 2 3 1 2 3 1 2 3]);
+labels = labels(:);
+for k = 1:9
+    text(x(k), y(k), labels(k), 'Color', 'w')
+end
+title(name)
 
 % save figure
-filename = [pct_MVC,'% MVC colormaps.png'];
+filename = [name,'.png'];
 exportgraphics(gcf, filename)
 close
+
 end
