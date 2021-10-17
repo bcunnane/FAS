b5b497c2c5d9ee3e1e8d53af81914451b3a694f4
Brandon Cunnane <brandon.cunnane@gmail.com>
2021-10-17 15:25:42 -0700

    added fiber aligned strain function

--- fas_analysis_main.m
+++ fas_analysis_main.m
@@ -1,58 +1,83 @@
 %% FAS analysis main
-%   main script for fiber aligned strain & strain rate analysis
-% load('Results.mat')
+% main script for fiber aligned strain & strain rate analysis
+%   selected slice is always #2 as it is in the center of the volume 
 
-pcts = ["60","40","30"];
-
-%% get muscle ROI outlines
 series = 2;
+pcts = ["60","40","30"];
+mag_ims = squeeze(Data(series).m(:,:,2,:));
+% mg_roi = get_roi(mag_ims(:,:,8));
+mg_mask = poly2mask(mg_roi(:,1),mg_roi(:,2),256,256);
 
-mag_ims = squeeze(Data(series).m(:,:,2,:)); %first frame mag im for middle slice (#2)
-
-disp('ID mg')
-mg_roi = get_roi(mag_ims(:,:,8));
-
-disp('ID sol')
-sol_roi = get_roi(mag_ims(:,:,8));
 %% Strain & strain rate Eigenvector (ev) colormaps
 for v = 1:3
-%     % arrange ev data for plotting
-%     ev = squeeze(Data(series).strain.SR_vector(:,:,:,:,:,v)); % select ev
-%     ev = permute(ev,[1,2,4,3]); % [pix pix rgb frames]
-%     ev = ev(:,:,[3 1 2],:); %set rgb to zxy
-%     ev = abs(ev);
-%     
-%     mg_cmaps = ev_colormaps(ev, mg_roi, mag_ims);
-%     sol_cmaps = ev_colormaps(ev, sol_roi, mag_ims);
-%     
-%     %display
-%     montage(mg_cmaps, 'Size',[3 6], 'ThumbnailSize',[])
-%     exportgraphics(gcf,['SR MG ',char(pcts(series)),'% MVC EV',num2str(v),'.png'])
-%     close
-%     
-%     montage(sol_cmaps, 'Size',[3 6], 'ThumbnailSize',[])
-%     exportgraphics(gcf,['SR SOL ',char(pcts(series)),'% MVC EV',num2str(v),'.png'])
-%     close
-end
-%% DTI colormaps
-all_dti_evs = [];
-mag = mag_ims(:,:,1);
-for v = 3:-1:1
     % arrange ev data for plotting
-    ev = squeeze(Data(series).dti_vec(:,:,2,:,v)); % select ev
+    ev = squeeze(Data(series).strain.SR_vector(:,:,:,:,:,v)); % select ev
+    ev = permute(ev,[1,2,4,3]); % [pix pix rgb frames]
     ev = ev(:,:,[3 1 2],:); %set rgb to zxy
     ev = abs(ev);
-    all_dti_evs(:,:,:,v) = ev;
+    
+    mg_cmaps = ev_colormaps(ev, mg_mask, mag_ims);
+    
+    %display
+    montage(mg_cmaps, 'Size',[3 6], 'ThumbnailSize',[])
+    exportgraphics(gcf,['SR MG ',char(pcts(series)),'% MVC EV',num2str(v),'.png'])
+    close
 end
 
-%display
-montage(all_dti_evs, 'Size',[1 3], 'ThumbnailSize',[])
-exportgraphics(gcf,'DTI EV123.png')
-close
-%% Functions
-function cmaps = ev_colormaps(ev,roi,mag_ims)
+%% fiber aligned strain
+% change coordinates from xyz to zxy
+L = change_tensor_coords(Data(series).strain.L);
+SR = change_tensor_coords(Data(series).strain.SR);
 
-roi_mask = poly2mask(roi(:,1),roi(:,2),size(ev,1),size(ev,2));
+% align to DTI fibers
+v = 3;
+dti_vec = squeeze(Data(series).dti_vec(:,:,2,:,v));
+dti_vec = dti_vec(:,:,[3 1 2]);
+for fr = 17:-1:1
+    E(:,:,fr) = align_to_fibers(squeeze(L(:,:,fr,:,:)), dti_vec);
+    E_mg(:,:,fr) = E(:,:,fr) .* mg_mask;
+end
+
+% for fr = 17:-1:1
+%     subplot(3,6,fr)
+%     edges = [-.5:.025:-.2];
+%     histogram(E_mg(E_mg~=0),edges)
+% end
+%% Functions
+
+function new_tensor = change_tensor_coords(old_tensor)
+
+old_tensor = squeeze(old_tensor);
+old_tensor = old_tensor(:,:,:,[9 7 8 3 1 2 6 4 5]); % set xyz to zxy
+new_tensor = reshape(old_tensor,256,256,17,3,3);
+
+end
+
+
+function E = align_to_fibers(F,v)
+
+FT = F(:,:,[1 4 7 2 5 8 3 6 9]); %transpose
+
+% reshape for matrix multiplication
+F = reshape(F,256*256,3,3);
+F = permute(F,[2 3 1]);
+
+FT = reshape(FT,256*256,3,3);
+FT = permute(FT,[2 3 1]);
+
+v = reshape(v,256*256,3,1);
+v = permute(v,[2 3 1]);
+
+% align strain in direction of vector
+C = pagemtimes(FT,F);
+Cv = pagemtimes(C,v);
+E = .5 * (sum(v .* Cv) - 1);
+E = reshape(E,256,256);
+
+end
+
+
+function cmaps = ev_colormaps(ev,roi_mask,mag_ims)
 
 for c = 1:3
     for fr = size(mag_ims,3):-1:1
@@ -64,61 +89,3 @@ for c = 1:3
 end
 
 end
-
-
-function force_plot(lambdas,roi,force)
-
-% get strains
-roi_mask = poly2mask(roi(:,1),roi(:,2),size(lambdas,1),size(lambdas,2));
-for dim = 3:-1:1
-    for fr = 17:-1:1
-        data = squeeze(lambdas(:,:,dim,fr));
-        strains(dim,fr) = mean(data(roi_mask));
-    end
-end
-total_strain = dot(strains,strains).^(0.5);
-
-yyaxis left
-force = force(1:floor(600/16):end);
-%plot(force,'--y')
-plot(5:17,force(1:end-4),'--y')
-ylim([-40 100])
-ylabel('Force (N)')
-
-yyaxis right
-plot(1:17,strains(1,:),'-r',1:17,strains(2,:),'-g',1:17,strains(3,:),'-c',1:17,total_strain,'-w')
-ylim([-.4 1])
-ylabel('Strain')
-
-xlabel('Frame #')
-ax = gca;
-ax.Color = 'k';
-ax.XColor = 'w';
-ax.YAxis(1).Color = 'w';
-ax.YAxis(2).Color = 'w';
-set(gcf,'Color','k')
-legend('\color{yellow} force','\color{red} \epsilon_z','\color{green} \epsilon_x','\color{cyan} \epsilon_y','\color{white} \epsilon_a_l_l')
-end
-
-
-function animate(ims, name)
-% creates animaged gif of rgb images (ims)
-% assumes rgb image matrix is [pix, pix, rgb, frames]
-
-dt = .15; %delay time
-filename = [name, 'animation.gif'];
-
-for f = 1:size(ims,4)
-    im = ims(:,:,:,f);
-    [im,map] = rgb2ind(im,256);
-    
-    % Write to the GIF File
-    if f == 1
-        imwrite(im,map,filename,'gif','Loopcount',inf);
-    else
-        imwrite(im,map,filename,'gif','WriteMode','append','DelayTime',dt);
-    end
-end
-
-end
-
