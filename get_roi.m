85bc3d8dd57f79105dfb2b8b67b290d37bf08e3f
Brandon Cunnane <brandon.cunnane@gmail.com>
2021-10-22 09:59:19 -0700

    fn no longer needed

--- get_roi.m
+++
@@ -1,8 +0,0 @@
-function roi = get_roi(image)
-% returns ROI coordinates as [x, y]
-imshow(image,[])
-polygon = drawpolygon();
-roi = polygon.Position;
-roi = [roi;roi(1,:)]; %close roi by including first point again
-close all
-end
