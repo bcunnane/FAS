643cf5c3ce7b48cc5f6c296e0d78af0f249ba120
Brandon Cunnane <brandon.cunnane@gmail.com>
2022-04-25 11:18:48 -0700

    added square or polygon ROI option

---
+++ get_roi_fas.m
@@ -0,0 +1,16 @@
+function [roi_aves, roi_stds] = get_roi_fas(Evv, masks)
+% computes average and standard dev fiber aligned strain in ROI region
+
+% get mean and std data
+for fr = size(Evv,3):-1:1
+    % get ROI data
+    mask_frame = masks(:,:,fr);
+    Evv_frame = Evv(:,:,fr);
+    roi_data = Evv_frame(mask_frame);
+        
+    % calculate mean and std
+    roi_aves(fr) = mean(roi_data);
+    roi_stds(fr) = std(roi_data);
+end
+
+end
