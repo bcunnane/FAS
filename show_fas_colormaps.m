56fca74bd702dfb339a56f59877c9685dc4151e3
Brandon Cunnane <brandon.cunnane@gmail.com>
2022-04-25 11:19:50 -0700

    Shows strain and strain rate on one figure

--- show_fas_colormaps.m
+++ show_fas_colormaps.m
@@ -1,23 +1,55 @@
-function show_fas_colormaps(fas, mags, masks)
-% displays strain with colormap over magnitude images
-% displayed as 3x6 montage
+function show_fas_colormaps(D)
+% displays 3x6 montage of Fiber Aligned strain and strain rate as 
+% colormap overlaid on magnitude images
 
-% rearange data as tiles instead of frames
-fas = imtile(fas,'GridSize', [3 6]);
-mags = imtile(mags,'GridSize', [3 6]);
-masks = imtile(masks,'GridSize', [3 6]);
-
-% display background image as rgb
-mags_rgb = repmat(mags,[1,1,3]);
-imshow(mags_rgb)
-set(gca,'visible','off')
-hold on
-
-% display foreground image with colormap
-h = imshow(fas,[]);
-h.AlphaData = masks;
-colormap('jet')
-colorbar
-set(gca,'visible','off')
+figure
+tiledlayout(1,2,'TileSpacing','compact');
+for s = 1:2
+    % determine strain or strain rate
+    if s == 1
+        plot_title = 'Strain';
+        color_limits = [-0.3 0.3];
+    elseif s == 2
+        plot_title = 'Strain Rate';
+        color_limits = [-500 500];
+    end
+    
+    % get fiber aligned strain colormaps as tile image
+    fas = [];
+    mask = [];
+    mag = [];
+    for n = 1:3
+        % get FAS images
+        for v = [3 2 1]
+            fas = cat(3,fas,squeeze(D(n).Evv{s,v}(:,:,D(n).peak_fr)));
+        end
+        
+        % get mask & magnitude images
+        mask = cat(3,mask,repmat(D(n).mg(:,:,D(n).peak_fr),[1,3]));
+        mag = cat(3,mag,repmat(D(n).m(:,:,D(n).peak_fr),[1,3]));
+    end
+    
+    % convert to image tiles
+    fas = imtile(fas,'GridSize', [3 3]);
+    mask = imtile(mask,'GridSize', [3 1]);
+    mag = imtile(mag,'GridSize', [3 1]);
+    mag = repmat(mag,[1,1,3]); % convert to RGB image
+    
+    % display magnitude image as background
+    nexttile
+    imshow(mag,[])
+    hold on
+    
+    % overlay FAS colormap
+    h = imshow(fas,[]);
+    h.AlphaData = mask;
+    colormap('jet')
+    colorbar
+    set(gca,'visible','off')
+    caxis(color_limits)
+    title(plot_title)
+    
+end
+sgtitle([D(1).age,' ',D(1).name(1:end-7)])
 
 end
