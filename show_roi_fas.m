ec92edd99473327f4a2a34bb49c50ed7e30574b5
Brandon Cunnane <brandon.cunnane@gmail.com>
2022-04-26 12:34:28 -0700

    added roi to mask conversion

--- show_roi_fas.m
+++ show_roi_fas.m
@@ -1,35 +1,41 @@
-function show_ave_fas(aves, errs, mag_ims, masks, plot_name)
+function show_roi_fas(D)
 % creates plots of average strain in ROI with standard dev error bars
-% also shows the ROI on the magnitude image for the first frame
 
-figure('Position',[560 50 840 630])
-sgtitle([plot_name,' ave in ROI'])
+figure('WindowState','maximized')
+tiledlayout(3,6,'TileSpacing','compact');
 
-if contains(plot_name,'L ')
-    y_label = 'Strain';
-    y_limits = [-0.2 0.4];
-elseif contains(plot_name,'SR')
-    y_label = 'Strain Rate';
-    y_limits = [-1000 1000];
+for n = 1:length(D)
+    for s = 1:2
+        % determine strain or strain rate
+        if s==1
+            y_label = 'Strain';
+            y_limits = [-0.1 0.3];
+        elseif s==2
+            y_label = 'Strain Rate [s^-^1]';
+            y_limits = [-750 750];
+        end
+        
+        for v = [3 2 1]
+            % create plot
+            aves = D(n).Evv_aves{s,v};
+            errs = D(n).Evv_stds{s,v};
+            nexttile
+            errorbar(aves,errs,'o')
+            
+            % annotate plot
+            xlabel('Frame')
+            ylim(y_limits)
+            if v == 3
+                ylabel([D(n).name(end-5:end), newline, y_label])
+            else
+                ylabel(y_label)
+            end            
+            if n == 1
+                title(['proj on EV',num2str(v)])
+            end
+        end
+    end
 end
-
-% strain plots
-for v = 1:3
-    subplot(2,2,v)
-    errorbar(aves{v},errs{v},'o')
-    title(['Proj on EV',num2str(v)])
-    xlabel('Frame #')
-    
-    ylabel(y_label)
-    ylim(y_limits)
-end
-
-% ROI
-im = mag_ims(:,:,1);
-mask = masks(:,:,1);
-im(mask) = max(im(:));
-subplot(2,2,4)
-imshow(im,[])
-
+sgtitle([D(1).age,' ',D(1).name(1:end-7)],'Interpreter', 'none')
 
 end
