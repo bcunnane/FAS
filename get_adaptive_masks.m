553ce1d7042b9b0b0fba162c6e8a3ab1e223eeb1
Brandon Cunnane <brandon.cunnane@gmail.com>
2022-04-26 09:12:38 -0700

    added square roi option

--- get_adaptive_masks.m
+++ get_adaptive_masks.m
@@ -1,7 +1,8 @@
-function masks = get_adaptive_masks(images, vx, vy)
+function masks = get_adaptive_masks(images, vx, vy, type)
 % prompts user to draw region of interest (ROI) on first image frame
 % tracks ROI motion over subsequent frames
-% returns matrix of mask of each ROI
+% returns matrix of mask of each ROI for each frame
+% type (p) polygone ROI (s) 7x7 square ROI
 
 % initialize
 RES = 1.1719;
@@ -12,10 +13,20 @@ num_frames = size(vx,3);
 dt = ones(num_frames-1,1)*0.136;
 
 % get ROI on first frame
-imshow(images(:,:,1),[])
-polygon = drawpolygon();
-roi = polygon.Position;
-roi = [roi;roi(1,:)]; %close roi by including first point again
+if type == 'p'
+    figure('Name','Draw Polygon ROI','NumberTitle','off')
+    imshow(images(:,:,1),[])
+    polygon = drawpolygon();
+    roi = polygon.Position;
+    %roi = [roi;roi(1,:)]; %close roi by including first point again
+elseif type == 's'
+    figure('Name','Select top left corner of 7x7 square ROI','NumberTitle','off')
+    imshow(images(:,:,1),[])
+    roi = ginput(1);
+    roi(2,:) = roi(1,:) + [7 0];
+    roi(3,:) = roi(1,:) + [7 7];
+    roi(4,:) = roi(1,:) + [0 7];
+end
 close
 
 % track ROI motion and get ROIs xs and ys for each image frame
